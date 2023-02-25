0de9a99129951eab7baa069e4add479cc3f006da
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2023-02-24 22:45:18 -0300

    :sparkles: seção factory method

---
+++ creational-patterns/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneFactory.java
@@ -0,0 +1,21 @@
+package br.com.cod3r.factory.apple.factory;
+
+import br.com.cod3r.factory.apple.model.IPhone;
+
+public abstract class IPhoneFactory {
+
+    public IPhone orderIphone(){
+        IPhone device = null;
+
+        device = createIphone();
+
+        device.getHardware();
+        device.assemble();
+        device.certificates();
+        device.pack();
+
+        return device;
+    }
+
+    protected abstract IPhone createIphone();
+}
